bd348e45dc5daed3b7bf62dbc83266904866c0c1
Jack Bennett <jackbennett0708@gmail.com>
2024-10-17 18:08:38 +0100

    Add Makefile for automation and include CSS distribution

---
+++ Makefile
@@ -0,0 +1,25 @@
+OUT_DIR := dist
+SRC_DIR := .
+
+PAGES_DIR := $(SRC_DIR)/pages
+STYLES_DIR := $(SRC_DIR)/style
+TPL_DIR := $(SRC_DIR)/tpl
+
+PYTHON := python
+
+# create list of templated HTML pages and other copied files
+GEN_PAGES := $(subst pages,dist,$(shell find $(PAGES_DIR) -name "*.html"))
+GEN_STYLES := $(subst style,dist,$(shell find $(STYLES_DIR) -name "*.css"))
+
+all: $(GEN_PAGES) $(GEN_STYLES)
+
+$(OUT_DIR):
+	mkdir -p $(OUT_DIR)
+
+# template each HTML file
+$(OUT_DIR)/%.html: $(PAGES_DIR)/%.html | $(OUT_DIR)
+	$(TPL_DIR)/expand.py "$<" "$@"
+
+# copy CSS
+$(OUT_DIR)/%.css: $(STYLES_DIR)/%.css | $(OUT_DIR)
+	cp "$<" "$@"
--- tpl/expand.py
+++ tpl/expand.py
@@ -11,41 +11,34 @@ import os
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("outdir",
+    parser.add_argument("infile",
                         type=str,
-                        help="path to output/dist directory")
+                        help="path to file to be templated")
+    parser.add_argument("outfile",
+                        type=str,
+                        help="path to output file")
 
     return parser.parse_args()
 
-def abort():
-    print("Stopping")
-    exit(1)
-
 ARGS = get_args()
-OUTDIR = ARGS.outdir
+INFILE = os.path.basename(ARGS.infile)
+OUTFILE = os.path.realpath(ARGS.outfile)
 
-# output directory must exist
-if not os.path.exists(OUTDIR):
-    print(f"Error: outdir '{OUTDIR}' does not exist on the fs")
-    abort()
+# directory must exist for the output file
+if not os.path.exists(os.path.dirname(OUTFILE)):
+    print(f"Error: non-existent parent directory of output file '{OUTFILE}'")
+    exit(1)
 
 PAGES_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "pages")
 TPL_DIR = os.path.dirname(os.path.realpath(__file__))
 
-print(f"Loading templates from: {TPL_DIR}")
-print(f"Rendering pages from: {PAGES_DIR}")
-
 pages_env = j2.Environment(loader=j2.FileSystemLoader(PAGES_DIR))
 tpl_loader = j2.FileSystemLoader(TPL_DIR)
 
-for page in pages_env.list_templates():
-    outfile = os.path.join(OUTDIR, page)
+# get template for INFILE to be rendered into OUTFILE
+tpl = pages_env.get_template(INFILE)
+tpl.environment.loader = tpl_loader # {%include%}'d templates sourced via tpl_loader
 
-    pagetpl = pages_env.get_template(page)
-    pagetpl.environment.loader = tpl_loader
-
-    res = pagetpl.render()
-    with open(outfile, "w", encoding="utf-8") as f:
-        f.write(res)
-
-    print(f"\t{page} -> {outfile}")
+res = tpl.render()
+with open(OUTFILE, "w", encoding="utf-8") as f:
+    f.write(res)
